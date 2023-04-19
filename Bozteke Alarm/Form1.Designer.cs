aa339fde505cf2cee9efc116c219b64388a5dff0
burakbozteke <51003315+burakbozteke@users.noreply.github.com>
2023-04-19 03:09:23 +0300

    Proje dosyalarının yüklenmesi
    Uygulama deposuna tüm proje kaynakları yüklendi

---
+++ Bozteke Alarm/Form1.Designer.cs
@@ -0,0 +1,390 @@
+﻿namespace Bozteke_Alarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AccessibleName = "Zamanlanacak eylem:";
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(0, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.AccessibleName = "Saat:";
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59",
+            "60",
+            "61",
+            "62",
+            "63",
+            "64",
+            "65",
+            "66",
+            "67",
+            "68",
+            "69",
+            "70",
+            "71",
+            "72",
+            "73",
+            "74",
+            "75",
+            "76",
+            "77",
+            "78",
+            "79",
+            "80",
+            "81",
+            "82",
+            "83",
+            "84",
+            "85",
+            "86",
+            "87",
+            "88",
+            "89",
+            "90",
+            "91",
+            "92",
+            "93",
+            "94",
+            "95",
+            "96",
+            "97",
+            "98",
+            "99",
+            "100"});
+            this.comboBox2.Location = new System.Drawing.Point(8, 8);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.AccessibleName = "Dakika:";
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59"});
+            this.comboBox3.Location = new System.Drawing.Point(16, 16);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 2;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.AccessibleName = "Saniye:";
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59"});
+            this.comboBox4.Location = new System.Drawing.Point(24, 24);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleName = "Hatırlatılıcak not:";
+            this.textBox1.Location = new System.Drawing.Point(32, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.AccessibleName = "Ayarlanan bilgiler:";
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 8;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Program arka planda çalışmaya devam ediyor.";
+            this.notifyIcon1.BalloonTipTitle = "Uyarı!";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Bozteke Alarm/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Collections;
+using System.Diagnostics;
+
+namespace Bozteke_Alarm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string surum = "1.0.0";
+        int islem, saat, dakika, saniye, hour, minute, second;
+        string animsatilacakNot, secilenIslem, veri, aIslem;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Bozteke Alarm V" + surum;
+            comboBox1.Items.Add(new DictionaryEntry("Bilgisayarı kapat", "-s -f -t 0"));
+            comboBox1.Items.Add(new DictionaryEntry("Bilgisayarı yeniden başlat", "-r -f -t 0"));
+                        comboBox1.Items.Add(new DictionaryEntry("Anımsatılacak bir not oluştur", "Anımsatılacak bir not oluştur"));
+            comboBox1.DisplayMember = "Key";
+            comboBox1.ValueMember = "Value";
+            comboBox1.DataSource = comboBox1.Items;
+
+
+            button1.Text = "Ayarla";
+            textBox1.Enabled = false;
+            textBox1.Visible = false;
+            listBox1.Visible = false;
+            listBox1.Enabled = false;
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
+            comboBox4.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (button1.Text == "Durdur")
+                {
+                    timer1.Stop();
+                    hour = 0;
+                    minute = 0;
+                    second = 0;
+                    saat = 0;
+                    dakika = 0;
+                    saniye = 0;
+                    button1.Text = "Ayarla";
+                    listBox1.Items.Clear();
+                    listBox1.Enabled = false;
+                    listBox1.Visible = false;
+                    comboBox1.SelectedIndex = 0;
+                    comboBox2.SelectedIndex = 0;
+                    comboBox3.SelectedIndex = 0;
+                    comboBox4.SelectedIndex = 0;
+                    textBox1.Text = "";
+                    comboBox1.Focus();
+                    return;
+                }
+                else
+                {
+                    if (textBox1.Text == "")
+                    {
+                        animsatilacakNot = "Tanımlı mesaj yok.";
+                    }
+                    else
+                    {
+                        animsatilacakNot = textBox1.Text;
+                    }
+                    islem = comboBox1.SelectedIndex;
+                    saat = comboBox2.SelectedIndex;
+                    dakika = comboBox3.SelectedIndex;
+                    saniye = comboBox4.SelectedIndex;
+                    if (saat == 0 && dakika == 0 && saniye == 0)
+                    {
+                        secilenIslem = ((DictionaryEntry)comboBox1.SelectedItem).Key.ToString();
+                        MessageBox.Show(secilenIslem + " işlemi için herhangi bir zaman ayarı yapmadınız. Bu işlemi sihirli bir şekilde mi gerçekleştirmek istiyorsunuz?", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        comboBox2.Focus();
+                        return;
+                    }
+                    else
+                    {
+                        secilenIslem = ((DictionaryEntry)comboBox1.SelectedItem).Value.ToString();
+                        listBox1.Visible = true;
+                        listBox1.Enabled = true;
+                        listBox1.Items.Clear();
+                        if (islem == 2)
+                        {
+                            listBox1.Items.Add("İşlem: " + ((DictionaryEntry)comboBox1.SelectedItem).Key.ToString());
+                            listBox1.Items.Add("Ayarlanan zaman: " + saat.ToString("00") + ":" + dakika.ToString("00") + ":" + saniye.ToString("00"));
+                            listBox1.Items.Add("Anımsatılacak not: " + animsatilacakNot);
+                        }
+                        else
+                        {
+                            listBox1.Items.Add("İşlem: " + ((DictionaryEntry)comboBox1.SelectedItem).Key.ToString());
+                            listBox1.Items.Add("Ayarlanan zaman: " + saat.ToString("00") + ":" + dakika.ToString("00") + ":" + saniye.ToString("00"));
+                        }
+                        listBox1.Focus();
+                        listBox1.SelectedIndex = 0;
+                    }
+                    button1.Text = "Durdur";
+                    timer1.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Kritik hata!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            second = second + 1;
+            if (second == 60)
+            {
+                second = 0;
+                minute = minute + 1;
+            }
+            if (minute == 60)
+            {
+                minute = 0;
+                hour = hour + 1;
+            }
+            if (hour == saat && minute == dakika && second == saniye)
+            {
+                listBox1.Items.Clear();
+                listBox1.Visible = false;
+                listBox1.Enabled = false;
+                comboBox1.SelectedIndex = 0;
+                comboBox2.SelectedIndex = 0;
+                comboBox3.SelectedIndex = 0;
+                comboBox4.SelectedIndex = 0;
+                textBox1.Text = "";
+                textBox1.Visible = false;
+                textBox1.Enabled = false;
+                button1.Text = "Ayarla";
+                hour = 0;
+                minute = 0;
+                second = 0;
+                saat = 0;
+                dakika = 0;
+                saniye = 0;
+                if (islem == 2)
+                {
+                    MessageBox.Show("Hatırlatılacak not: " + animsatilacakNot, "Hatırlatıcı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    timer1.Stop();
+                    timer1.Enabled = false;
+                    return;
+                }
+                else
+                {
+                    timer1.Stop();
+                    timer1.Enabled = false;
+                    KomutYurut(secilenIslem);
+                                    }
+            }
+                    }//timer nesnesinin sonu
+
+        public void KomutYurut(string calistirilacakKomut)
+        {
+            Process komut = new Process();
+            ProcessStartInfo baslangicBilgisi = new ProcessStartInfo();
+            baslangicBilgisi.FileName = "shutdown";
+            baslangicBilgisi.Arguments = calistirilacakKomut;
+            baslangicBilgisi.UseShellExecute = false;
+            baslangicBilgisi.RedirectStandardOutput = true;
+            baslangicBilgisi.RedirectStandardError = true;
+            baslangicBilgisi.CreateNoWindow = true;
+            komut.StartInfo = baslangicBilgisi;
+            komut.Start();
+
+        }//fonksiyon sonu
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(3000);
+                if (timer1.Enabled == false)
+                {
+                    notifyIcon1.Text = "Bozteke Alarm V" + surum;
+                    return;
+                }
+                else
+                {
+                    timer2.Enabled = true;
+                    timer2.Start();
+                }
+            }
+        }//form resize olayının sonu
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+            timer2.Stop();
+            timer2.Enabled = false;
+            notifyIcon1.Visible = false;
+        }//notifyIcon1 double click olayı sonu
+
+         private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (islem == 0)
+            {
+                notifyIcon1.Text = "Bilgisayar kapatma için geçen zaman " + hour.ToString("00") + ":" + minute.ToString("00") + ":" + second.ToString("00");
+                return;
+            }
+            if (islem == 1)
+            {
+                notifyIcon1.Text = "Yeniden başlatma için geçen zaman: " + hour.ToString("00") + ":" + minute.ToString("00") + ":" + second.ToString("00");
+                return;
+            }
+            if (islem == 2)
+            {
+                notifyIcon1.Text = "Anımsatıcı için geçen zaman: " + hour.ToString("00") + ":" + minute.ToString("00") + ":" + second.ToString("00");
+                return;
+            }
+        }//timer2 tick olayının sonu
+
+                private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 2)
+            {
+                textBox1.Visible = true;
+                textBox1.Enabled = true;
+            }
+            else
+            {
+                textBox1.Visible = false;
+                textBox1.Enabled = false;
+            }
+        }//combobox1 changed olayının sonu
+
+                    }//Proje kod kapsamı name space sonu
+}//program sonu
